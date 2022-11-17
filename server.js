6ae911ddc48000f7a1e3f4f1e77cbcd59094569c
Thomas Morris <thomas@clearstep.health>
2022-11-17 15:22:29 -0600

    Add response header

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const request = require('request');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 3000);
 
 http.createServer(function (req, res) {
-    if(req.url.match(/path=(.*)/)) {
-        res.setHeader("access-control-allow-origin", "*");
-        request.get(req.url.match(/path=(.*)/)[1]).pipe(res);
-    }else{
-        res.statusCode = 404;
-
-        res.end("More info at https://github.com/cDima/cors");
-    }
+    res.setHeader("access-control-allow-origin", "*");
 }).listen(process.env.PORT || 3000);
