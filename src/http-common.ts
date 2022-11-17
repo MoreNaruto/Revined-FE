33f7dbccbd3efa3ca85b2f03e84ef0d8bebe49e8
Thomas Morris <thomas@clearstep.health>
2022-11-17 16:53:09 -0600

    Remove preflight

--- src/http-common.ts
+++ src/http-common.ts
@@ -10,11 +10,7 @@ interface CSRFTokenResponse {
 
 // Add CSRF for all endpoint: https://devdojo.com/ketonemaniac/doing-spring-securitys-csrf-tokens-the-right-way-with-react
 export function post(path: string, data: any, cookie: string): AxiosPromise {
-    return axios.get(`${baseApiUrl}/csrf`, {
-        headers: {
-            'User-Rackd-Cookie': cookie
-        }
-    })
+    return axios.get(`${baseApiUrl}/csrf`)
         .then((tokenResp: AxiosResponse<CSRFTokenResponse>) => {
             const config = {
                 headers: {
