47e26530e198cd8c35270c00682a0dfb88362203
Thomas Morris <thomas@clearstep.health>
2022-11-17 22:23:52 -0600

    Remove headers

--- src/http-common.ts
+++ src/http-common.ts
@@ -9,9 +9,6 @@ interface CSRFTokenResponse {
     headerName: string
 }
 
-axios.defaults.headers.common['Authorization'] = "Bearer " + localStorage.getItem("token") ?? "";
-axios.defaults.headers.common['Accept'] = "application/json";
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = "*";
 axios.defaults.headers.common['Access-Control-Max-Age'] = 600;
 
 // Add CSRF for all endpoint: https://devdojo.com/ketonemaniac/doing-spring-securitys-csrf-tokens-the-right-way-with-react
@@ -22,7 +19,8 @@ export function post(path: string, data: any, cookie: string): AxiosPromise {
                 headers: {
                     'Content-Type': "application/json",
                     'X-XSRF-TOKEN': tokenResp.data.token,
-                    'User-Rackd-Cookie': cookie
+                    'User-Rackd-Cookie': cookie,
+                    'Authorization': "Bearer " + localStorage.getItem("token") ?? ""
                 },
 
             };
